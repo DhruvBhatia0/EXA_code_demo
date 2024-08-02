b5bc5f5d3fd76570d132252190f76fb640f4f8ca
Dhruv Bhatia <dhruv.bhatia.j@gmail.com>
2024-08-01 21:38:56 -0400

    subtract doesnt work rn

--- grid_search/src/main.rs
+++ grid_search/src/main.rs
@@ -9,3 +9,37 @@ pub struct HashKey<const N: usize>([u32; N]);
 
 #[derive(Copy, Clone)]
 pub struct Vector<const N: usize>(pub [f32; N]);
+
+impl<const N: usize> Vector<N> {
+    pub fn dot_prod(&self, vector: &Vector<N>) -> f32 {
+        // use zip
+        return self.0.iter().zip(vector.0).map(|(a, b)| a * b).sum::<f32>();
+    }
+
+    pub fn subtract(&self, vector: &Vector<N>) -> Vector<N> {
+        return Vector::<N>(self.0.zip(victor.0).map(|a, b| b - a));
+    }
+}
+
+fn main() {
+    // Test vectors
+    let v1 = Vector([1.0, 2.0, 3.0]);
+    let v2 = Vector([4.0, 5.0, 6.0]);
+
+    // Test dot product
+    let dot_result = v1.dot_prod(&v2);
+    println!("Dot product of {:?} and {:?} is {}", v1.0, v2.0, dot_result);
+
+    // Test subtraction
+    let sub_result = v2.subtract(&v1);
+    println!(
+        "Subtraction result of {:?} - {:?} is {:?}",
+        v2.0, v1.0, sub_result.0
+    );
+
+    // Verify results
+    assert_eq!(dot_result, 32.0); // 1*4 + 2*5 + 3*6 = 32
+    assert_eq!(sub_result.0, [3.0, 3.0, 3.0]); // [4-1, 5-2, 6-3] = [3, 3, 3]
+
+    println!("All tests passed!");
+}
